1484ac574cc0d3dfa3bfa7e48a54548b5a40abfd
SabrinaG <sabrinag@epidataconsulting.com>
2025-05-04 21:09:33 +0200

    cleanup

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,3 @@
 <template>
-  <div>
-    <NuxtLink to="/login">
-      <UButton color="primary">Login</UButton>
-    </NuxtLink>
-    <NuxtLink to="/registro">
-      <UButton color="primary">Register</UButton>
-    </NuxtLink>
-    <NuxtLink to="/map">
-      <UButton color="primary">Map</UButton>
-    </NuxtLink>
-  </div>
+  <ParkingMap />
 </template>
-<script setup>
-const blobName = "merged_parking_weather"; // puede venir de un input o selección
-
-const { data, error } = await useFetch(`/api/blob`, {
-  query: { name: blobName },
-});
-
-if (data.value) {
-  console.log("Contenido del blob:", data.value.content);
-}
-</script>
--- pages/map.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <Map />
-  </div>
-</template>
-
-<script setup>
-import Map from "~/components/ParkingMap.client.vue";
-</script>
