599d81a7f3babfd754c3bf4b7a8a955382df109c
SabrinaG <sabrinag@epidataconsulting.com>
2025-05-15 17:54:32 +0200

    feature: update de id de chatling

--- app.vue
+++ app.vue
@@ -9,14 +9,14 @@ import { onMounted } from "vue";
 onMounted(() => {
   // Script inline: window.chtlConfig = {...}
   const configScript = document.createElement("script");
-  configScript.innerHTML = `window.chtlConfig = { chatbotId: "2558883383" };`;
+  configScript.innerHTML = `window.chtlConfig = { chatbotId: "1351377169" };`;
   document.head.appendChild(configScript);
 
   // Script externo
   const embedScript = document.createElement("script");
   embedScript.async = true;
   embedScript.src = "https://chatling.ai/js/embed.js";
-  embedScript.setAttribute("data-id", "2558883383");
+  embedScript.setAttribute("data-id", "1351377169");
   embedScript.id = "chtl-script";
   embedScript.type = "text/javascript";
   document.head.appendChild(embedScript);
